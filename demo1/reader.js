c41323fbc8b277a3e94fb3cabbd86d459d42a50b
alanzhang <alnzhang@futunn.com>
2019-05-14 21:43:16 +0800

    init

--- demo1/reader.js
+++ demo1/reader.js
@@ -6,6 +6,6 @@ fs.readFile(path.join(__dirname, './demo1.log'), function (err, data) {
     if (!err) {
         console.log(data); // 来看看Node里的Buffer对象长什么样子。
         var message = HelloWorld.decode(data);
-        console.log(message);
+        console.log(message.id,message.str);
     }
 });
--- demo1/writer.js
+++ demo1/writer.js
@@ -4,10 +4,17 @@ const path = require('path');
 
 let hw = new HelloWorld({
     'id': 101,
-    'str': 'Hello000000'
+    'str': 'Hello000000',
+    '34234524': 3452345245
 });
 
 let buffer = HelloWorld.encode(hw).finish();
+
+var message = HelloWorld.decode(buffer);
+var obj = HelloWorld.toObject(message);
+console.log(message);
+
+console.log(obj);
 
 fs.writeFile(path.join(__dirname,'./demo1.log'), buffer,err=>{
     if(!err) {
